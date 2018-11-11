bbfd6affb3bbc30933ae7ea50c48ffa0d572abee
Eduardo Heinen <eduardogheinen@gmail.com>
2018-11-11 22:01:57 +0100

    Added Miscellaneous and String stuffs

---
+++ miscellaneous/src/main/java/com/miscellaneous/string/AmbiguousMethod.java
@@ -0,0 +1,20 @@
+package com.miscellaneous.string;
+
+public class AmbiguousMethod {
+
+    public static void main(String[] args) {
+        // Ambiguous method call. Both foo match the call
+        // new MainString().foo(null);
+    }
+
+    public void foo(String s) {
+        System.out.println("String");
+    }
+
+    public void foo(StringBuffer s) {
+        System.out.println("StringBuffer");
+    }
+
+}
+
+
